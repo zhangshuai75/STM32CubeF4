23fb679e4a0ed442e48d4935cbefd509d6d6a801
zhang.shuai.stephen <zhang.shuai.stephen@vertiv.com>
2024-09-07 13:15:33 +0800

    v1.1.0

---
+++ Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.d
@@ -0,0 +1,50 @@
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.o: \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ include/stm32f4xx_hal_conf.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/stdint.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/machine/_default_types.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/sys/features.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmInstr.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmFunc.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4_simd.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+include/stm32f4xx_hal_conf.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/stdint.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/sys/features.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmInstr.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmFunc.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4_simd.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
---
+++ Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.d
@@ -0,0 +1,101 @@
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.o: \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ include/stm32f4xx_hal_conf.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/stdint.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/machine/_default_types.h \
+ /usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/sys/features.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmInstr.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmFunc.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4_simd.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ ../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+include/stm32f4xx_hal_conf.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/stdint.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/sys/features.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmInstr.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cmFunc.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Include/core_cm4_simd.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+../STM32Cube_FW_F4_V1.1.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
